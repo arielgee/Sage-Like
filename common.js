87afac795fab376a6181987fc278f9ba320d096f
arielgee <ariel.hazard@gmail.com>
2018-05-13 09:33:19 +0300

    commit test

--- common.js
+++ common.js
@@ -4,6 +4,8 @@
 ///
 let lzUtil = (function () {
 
+	let git_hub_commit_test = 242;
+
 	//////////////////////////////////////////////////////////////////////
 	String.prototype.format = function (args) {
 		let str = this;
