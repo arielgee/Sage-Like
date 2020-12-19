74a794e99d19762fa725b94186ec875225e9126c
arielgee <ariel.hazard@gmail.com>
2020-12-19 12:08:54 +0200

    Rewrite to String.prototype.htmlEntityToLiteral()
    First replace only the HTML '&amp;'  entity before hadling the other html entitiies. THis is to handle cases like: "&amp;#38;" that translate to "&#38;"  that translate to "&".
    Also added: "&#8211;": "—",

--- Sage-Like/common.js
+++ Sage-Like/common.js
@@ -711,14 +711,14 @@ let slPrototypes = (function() {
 	////////////////////////////////////////////////////////////////////////////////////
 	String.prototype.htmlEntityToLiteral = function() {
 		// this is NOT safe; may be used as an attack vector if result is displayed to user
-		return this.replace(String.prototype.htmlEntityToLiteral.regex, (matched) => {
+		return this.replaceAll("&amp;", "&").replace(String.prototype.htmlEntityToLiteral.regex, (matched) => {
 			return String.prototype.htmlEntityToLiteral.entities[matched];
 		});
 	};
 	String.prototype.htmlEntityToLiteral.entities = {
 		"&quot;": "\"",
 		"&apos;": "'",
-		"&amp;": "&",
+		//"&amp;": "&",
 		"&gt;": ">",
 		"&lt;": "<",
 		"&nbsp;": " ",
@@ -730,6 +730,7 @@ let slPrototypes = (function() {
 		"&#60;": "<",
 		"&#160;": " ",
 		"&#8195;": " ",
+		"&#8211;": "—",
 		"&reg;": "®",
 		"&copy;": "©",
 		"&trade;": "™",
