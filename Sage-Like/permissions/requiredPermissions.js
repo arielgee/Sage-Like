7fed1d7f5dbf025b883a0b7e3b5ab4047749ec16
arielgee <ariel.hazard@gmail.com>
2022-10-13 22:16:13 +0300

    requiredPermissions.js: Rewrite informational text in getInfoText()

--- Sage-Like/permissions/requiredPermissions.js
+++ Sage-Like/permissions/requiredPermissions.js
@@ -37,8 +37,9 @@ class RequiredPermissions {
 
 	//////////////////////////////////////////
 	getInfoText() {
-		return "Due to changes in the add-ons environment (Manifest V3) the optional permission " +
+		return "Due to changes in the add-ons environment (Manifest V3) the now optional permission " +
 				"<b>Access your data for all websites</b> is currently not allowed by the browser.<br><br>" +
+				"Without this permission, most feeds will probably not load as expected.<br><br>" +
 				"A website can set up its feeds in a way that does not require this permission. But many " +
 				"feeds are not, and the result is a network error caused by browser's restrictions (CORS).";
 	}
