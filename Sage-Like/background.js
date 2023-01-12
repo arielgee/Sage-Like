cd884051f62a31fc4e4910d5d1831140d0b10638
arielgee <ariel.hazard@gmail.com>
2023-01-12 13:01:20 +0200

    background.onTabsUpdated() improved
    Also debouncer Delay changed from 2000 ms to 900 ms

--- Sage-Like/background.js
+++ Sage-Like/background.js
@@ -175,13 +175,21 @@
 
 	////////////////////////////////////////////////////////////////////////////////////
 	function onTabsUpdated(tabId, changeInfo, tab) {
-		if (changeInfo.status === "complete" && IsAllowedForFeedDetection(tab.url)) {
-			hidePageAction(tabId);
+
+		// When the change is the status of the tab. Can be either loading or complete.
+		if ( !!changeInfo.status && IsAllowedForFeedDetection(tab.url) ) {
+
 			clearTimeout(m_onTabsUpdatedDebouncersMap.get(tabId));
-			m_onTabsUpdatedDebouncersMap.set(tabId, setTimeout(() => {
-				handleTabChangedState(tabId);
-				m_onTabsUpdatedDebouncersMap.delete(tabId);
-			}, 2000));
+
+			if (changeInfo.status === "complete") {
+
+				hidePageAction(tabId);
+
+				m_onTabsUpdatedDebouncersMap.set(tabId, setTimeout(() => {
+					handleTabChangedState(tabId);
+					m_onTabsUpdatedDebouncersMap.delete(tabId);
+				}, 900));
+			}
 		}
 	}
 
